d5cf80b1f81114dc124f8ab9e2c7058405e2f45b
Roberto Evangelista <robertoevangelistasf@gmail.com>
2023-02-17 00:16:40 +0000

    feat: add deck list view controller file

---
+++ MagicalDeckBuilder/Scenes/DeckList/DeckListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DeckListViewController.swift
+//  MagicalDeckBuilder
+//
+//  Created by Roberto Evangelista on 17/02/23.
+//
+
+import UIKit
+
+// MARK: - Protocol
+protocol DeckListViewControllerProtocol: UIViewController {
+    
+}
+
+// MARK: - Class
+final class DeckListViewController: UIViewController, DeckListViewControllerProtocol {
+
+}
