62d14d473d3ba345ab8d59fdfe2045a6571618a3
Roberto Evangelista <robertoevangelistasf@gmail.com>
2023-02-17 23:27:36 +0000

    feat: add Margins struct to create some standards sizes

---
+++ MagicalDeckBuilder/Assets/Margins.swift
@@ -0,0 +1,16 @@
+//
+//  Margins.swift
+//  MagicalDeckBuilder
+//
+//  Created by Roberto Evangelista on 17/02/23.
+//
+
+import Foundation
+
+struct Margins {
+    static let extraSmall: CGFloat = 4
+    static let small: CGFloat = 8
+    static let regular: CGFloat = 12
+    static let large: CGFloat = 16
+    static let extraLarge: CGFloat = 20
+}
