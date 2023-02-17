892c2b85cc586c44e82ac9e751a8fec5507069fb
Roberto Evangelista <robertoevangelistasf@gmail.com>
2023-02-17 00:17:29 +0000

    feat: add DeckListViewModel

---
+++ MagicalDeckBuilder/Scenes/DeckList/DeckListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DeckListViewModel.swift
+//  MagicalDeckBuilder
+//
+//  Created by Roberto Evangelista on 17/02/23.
+//
+
+import Foundation
+
+// MARK: - Protocol
+protocol DeckListViewModelProtocol {
+    
+}
+
+// MARK: - Class
+final class DeckListViewModel: DeckListViewModelProtocol {
+
+}
