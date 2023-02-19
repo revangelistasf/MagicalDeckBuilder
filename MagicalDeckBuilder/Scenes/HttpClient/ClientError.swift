51e4cd11e018405b8144a78ed0fdaf44385aa52b
Roberto Evangelista <robertoevangelistasf@gmail.com>
2023-02-19 01:15:00 +0000

    feat: add client error

---
+++ MagicalDeckBuilder/Scenes/HttpClient/ClientError.swift
@@ -0,0 +1,13 @@
+//
+//  ClientError.swift
+//  MagicalDeckBuilder
+//
+//  Created by Roberto Evangelista on 19/02/23.
+//
+
+import Foundation
+
+enum ClientError: Error {
+    case invalidRequest
+    case unexpected
+}
